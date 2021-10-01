a12441554bd0cc74ef39fc657e453af44002557a
xanderaerts <xanderaerts@gmail.com>
2021-10-01 13:13:11 +0200

    sceenrun

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
-import pygame
+import pygame as pg
+import os
 
-pygame.init()
+pg.init()
 
+
+#Constands 
+#COLORS
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 GREEN = (0,255,0)
 RED = (255,0,0)
+BLUE = (0,0,255)
 
-size = (700,500)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("My First Game")
+WIDTH,HEIGHT = 900,500
+FPS = 60
 
-carrOn = True
-clock = pygame.time.Clock()
 
-while carrOn:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            carrOn = False
+#window settings
+WIN = pg.display.set_mode((WIDTH,HEIGHT))
+pg.display.set_caption("Quiz game")
 
-    screen.fill(WHITE)
+#Functions
+def draw_window():
+    WIN.fill(WHITE)
+    pg.display.update()
+    
 
-    pygame.display.flip()
 
-    clock.tick(60)
 
-pygame.quit()
+#main loop of the game
+def main():
+    #clock = pg.time.clock()
+    run = True
+    while(run):
+        #clock.tick(FPS)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+        draw_window()
+       
+
+
+    pg.quit()
+
+
+if __name__ == "__main__": #runs the game only when this file is run, not when imported by another file
+    main()
+
+
+pg.quit()
